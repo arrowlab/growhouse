0f74cebe5453ea28984bc4d5624b2bf72d0a1752
arrowlab <arrowlabsg@gmail.com>
2022-01-07 21:43:51 +0800

    Add Radial Gauge

--- growhouse_dashboard/growhouse_dashboard/lib/dashboard.dart
+++ growhouse_dashboard/growhouse_dashboard/lib/dashboard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:growhouse_dashboard/custom_data.dart';
 import 'package:growhouse_dashboard/database.dart';
 import 'package:provider/provider.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -14,6 +15,7 @@ class _DashboardState extends State<Dashboard> {
   double? redValue;
   double? greenValue;
   double? blueValue;
+
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
@@ -37,24 +39,261 @@ class _DashboardState extends State<Dashboard> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  DataWindow(
-                    title: "Soil pH",
-                    value: data.pH.toString(),
-                  ),
+                  SfRadialGauge(axes: <RadialAxis>[
+                    RadialAxis(
+                        startAngle: 270,
+                        endAngle: 270,
+                        minimum: 0,
+                        maximum: 14,
+                        interval: 1,
+                        radiusFactor: 0.4,
+                        showAxisLine: false,
+                        showLastLabel: false,
+                        minorTicksPerInterval: 4,
+                        majorTickStyle: MajorTickStyle(
+                            length: 8, thickness: 3, color: Colors.white),
+                        minorTickStyle: MinorTickStyle(
+                            length: 3, thickness: 1.5, color: Colors.grey),
+                        axisLabelStyle: GaugeTextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14),
+                        onLabelCreated: labelCreated),
+                    RadialAxis(
+                        minimum: 0,
+                        maximum: 14,
+                        labelOffset: 30,
+                        axisLineStyle: AxisLineStyle(
+                            thicknessUnit: GaugeSizeUnit.factor,
+                            thickness: 0.03),
+                        majorTickStyle: MajorTickStyle(
+                            length: 6, thickness: 4, color: Colors.white),
+                        minorTickStyle: MinorTickStyle(
+                            length: 3, thickness: 3, color: Colors.white),
+                        axisLabelStyle: GaugeTextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14),
+                        ranges: <GaugeRange>[
+                          GaugeRange(
+                              startValue: 0,
+                              endValue: 14,
+                              sizeUnit: GaugeSizeUnit.factor,
+                              startWidth: 0.03,
+                              endWidth: 0.03,
+                              gradient: SweepGradient(colors: const <Color>[
+                                Colors.yellow,
+                                Colors.green,
+                                Colors.red
+                              ], stops: const <double>[
+                                0.0,
+                                0.5,
+                                1
+                              ]))
+                        ],
+                        pointers: <GaugePointer>[
+                          NeedlePointer(
+                              value: data.pH,
+                              needleLength: 0.95,
+                              enableAnimation: true,
+                              animationType: AnimationType.ease,
+                              needleStartWidth: 1.5,
+                              needleEndWidth: 6,
+                              needleColor: Colors.red,
+                              knobStyle: KnobStyle(
+                                  knobRadius: 0.09,
+                                  sizeUnit: GaugeSizeUnit.factor))
+                        ],
+                        annotations: <GaugeAnnotation>[
+                          GaugeAnnotation(
+                              widget: Container(
+                                  child: Column(children: <Widget>[
+                                Text(data.pH.toString(),
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold)),
+                                SizedBox(height: 100),
+                                Text('pH',
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold))
+                              ])),
+                              angle: 90,
+                              positionFactor: 0.75)
+                        ])
+                  ]),
                   SizedBox(
                     width: 50,
                   ),
-                  DataWindow(
-                    title: "Humidity",
-                    value: data.humidity.toString(),
-                  ),
+                  SfRadialGauge(axes: <RadialAxis>[
+                    RadialAxis(
+                        startAngle: 270,
+                        endAngle: 270,
+                        minimum: 0,
+                        maximum: 1,
+                        interval: 0.1,
+                        radiusFactor: 0.4,
+                        showAxisLine: false,
+                        showLastLabel: false,
+                        minorTicksPerInterval: 4,
+                        majorTickStyle: MajorTickStyle(
+                            length: 8, thickness: 3, color: Colors.white),
+                        minorTickStyle: MinorTickStyle(
+                            length: 3, thickness: 1.5, color: Colors.grey),
+                        axisLabelStyle: GaugeTextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14),
+                        onLabelCreated: labelCreated),
+                    RadialAxis(
+                        minimum: 0,
+                        maximum: 1,
+                        labelOffset: 30,
+                        axisLineStyle: AxisLineStyle(
+                            thicknessUnit: GaugeSizeUnit.factor,
+                            thickness: 0.03),
+                        majorTickStyle: MajorTickStyle(
+                            length: 6, thickness: 4, color: Colors.white),
+                        minorTickStyle: MinorTickStyle(
+                            length: 3, thickness: 3, color: Colors.white),
+                        axisLabelStyle: GaugeTextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14),
+                        ranges: <GaugeRange>[
+                          GaugeRange(
+                              startValue: 0,
+                              endValue: 200,
+                              sizeUnit: GaugeSizeUnit.factor,
+                              startWidth: 0.03,
+                              endWidth: 0.03,
+                              gradient: SweepGradient(colors: const <Color>[
+                                Colors.red,
+                                Colors.yellow,
+                                Colors.green,
+                              ], stops: const <double>[
+                                0.0,
+                                0.5,
+                                1
+                              ]))
+                        ],
+                        pointers: <GaugePointer>[
+                          NeedlePointer(
+                              value: data.humidity,
+                              needleLength: 0.95,
+                              enableAnimation: true,
+                              animationType: AnimationType.ease,
+                              needleStartWidth: 1.5,
+                              needleEndWidth: 6,
+                              needleColor: Colors.red,
+                              knobStyle: KnobStyle(
+                                  knobRadius: 0.09,
+                                  sizeUnit: GaugeSizeUnit.factor))
+                        ],
+                        annotations: <GaugeAnnotation>[
+                          GaugeAnnotation(
+                              widget: Container(
+                                  child: Column(children: <Widget>[
+                                Text(data.humidity.toString(),
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold)),
+                                SizedBox(height: 100),
+                                Text('%',
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold))
+                              ])),
+                              angle: 90,
+                              positionFactor: 0.75)
+                        ])
+                  ]),
                   SizedBox(
                     width: 50,
                   ),
-                  DataWindow(
-                    title: "Temperature",
-                    value: data.temperature.toString(),
-                  ),
+                  SfRadialGauge(axes: <RadialAxis>[
+                    RadialAxis(
+                        startAngle: 270,
+                        endAngle: 270,
+                        minimum: 0,
+                        maximum: 40,
+                        interval: 10,
+                        radiusFactor: 0.4,
+                        showAxisLine: false,
+                        showLastLabel: false,
+                        minorTicksPerInterval: 4,
+                        majorTickStyle: MajorTickStyle(
+                            length: 8, thickness: 3, color: Colors.white),
+                        minorTickStyle: MinorTickStyle(
+                            length: 3, thickness: 1.5, color: Colors.grey),
+                        axisLabelStyle: GaugeTextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14),
+                        onLabelCreated: labelCreated),
+                    RadialAxis(
+                        minimum: 0,
+                        maximum: 40,
+                        labelOffset: 30,
+                        axisLineStyle: AxisLineStyle(
+                            thicknessUnit: GaugeSizeUnit.factor,
+                            thickness: 0.03),
+                        majorTickStyle: MajorTickStyle(
+                            length: 6, thickness: 4, color: Colors.white),
+                        minorTickStyle: MinorTickStyle(
+                            length: 3, thickness: 3, color: Colors.white),
+                        axisLabelStyle: GaugeTextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14),
+                        ranges: <GaugeRange>[
+                          GaugeRange(
+                              startValue: 0,
+                              endValue: 200,
+                              sizeUnit: GaugeSizeUnit.factor,
+                              startWidth: 0.03,
+                              endWidth: 0.03,
+                              gradient: SweepGradient(colors: const <Color>[
+                                Colors.blue,
+                                Colors.green,
+                                Colors.red
+                              ], stops: const <double>[
+                                0.0,
+                                0.5,
+                                1
+                              ]))
+                        ],
+                        pointers: <GaugePointer>[
+                          NeedlePointer(
+                              value: data.temperature,
+                              needleLength: 0.95,
+                              enableAnimation: true,
+                              animationType: AnimationType.ease,
+                              needleStartWidth: 1.5,
+                              needleEndWidth: 6,
+                              needleColor: Colors.red,
+                              knobStyle: KnobStyle(
+                                  knobRadius: 0.09,
+                                  sizeUnit: GaugeSizeUnit.factor))
+                        ],
+                        annotations: <GaugeAnnotation>[
+                          GaugeAnnotation(
+                              widget: Container(
+                                  child: Column(children: <Widget>[
+                                Text(data.temperature.toString(),
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold)),
+                                SizedBox(height: 100),
+                                Text('°C',
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold))
+                              ])),
+                              angle: 90,
+                              positionFactor: 0.75)
+                        ])
+                  ]),
                 ],
               ),
               SizedBox(
@@ -212,6 +451,27 @@ class _DashboardState extends State<Dashboard> {
       );
     }
   }
+
+  @override
+  void labelCreated(AxisLabelCreatedArgs args) {
+    if (args.text == '0') {
+      args.text = '';
+      args.labelStyle = GaugeTextStyle(
+          color: Colors.red, fontWeight: FontWeight.bold, fontSize: 14);
+    } else if (args.text == '10')
+      args.text = '';
+    else if (args.text == '20')
+      args.text = 'E';
+    else if (args.text == '30')
+      args.text = '';
+    else if (args.text == '40')
+      args.text = 'S';
+    else if (args.text == '50')
+      args.text = '';
+    else if (args.text == '60')
+      args.text = 'W';
+    else if (args.text == '70') args.text = '';
+  }
 }
 
 class DataWindow extends StatelessWidget {
@@ -233,8 +493,18 @@ class DataWindow extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Text(title!, style: TextStyle(fontSize: 20,),),
-          Text(value!, style: TextStyle(fontSize: 15,),),
+          Text(
+            title!,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            value!,
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
         ],
       ),
     );
